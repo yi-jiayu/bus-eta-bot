2196d1b0bed7a648d68a838d1f022c4707b10cb6
Jiayu Yi <yi-jiayu@users.noreply.github.com>
2017-05-24 12:06:06 +0800

    Extract answerCallbackQuery method

--- callbackquery.go
+++ callbackquery.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"sync"
 
@@ -17,6 +18,50 @@ var callbackQueryHandlers = map[string]CallbackQueryHandler{
 
 // CallbackQueryHandler is a handler for callback queries
 type CallbackQueryHandler func(ctx context.Context, bot *BusEtaBot, cbq *tgbotapi.CallbackQuery) error
+
+func answerCallbackQuery(bot *BusEtaBot, c tgbotapi.Chattable, answer tgbotapi.CallbackConfig) error {
+	errs := make([]error, 0)
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	errChan := make(chan error, 2)
+	go func() {
+		for err := range errChan {
+			errs = append(errs, err)
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+
+		_, err := bot.Telegram.Send(c)
+		if err != nil {
+			errChan <- err
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+
+		_, err := bot.Telegram.AnswerCallbackQuery(answer)
+		if err != nil {
+			errChan <- err
+		}
+	}()
+
+	wg.Wait()
+	close(errChan)
+
+	switch len(errs) {
+	case 1:
+		return errs[0]
+	case 2:
+		return errors.New(errs[0].Error() + "\n" + errs[1].Error())
+	default:
+		return nil
+	}
+}
 
 // RefreshCallbackHandler handles the callback for the Refresh button on an eta message.
 func RefreshCallbackHandler(ctx context.Context, bot *BusEtaBot, cbq *tgbotapi.CallbackQuery) error {
@@ -60,44 +105,16 @@ func RefreshCallbackHandler(ctx context.Context, bot *BusEtaBot, cbq *tgbotapi.C
 		},
 	}
 
+	answer := tgbotapi.NewCallback(cbq.ID, "Etas updated!")
+
 	if cbq.Message != nil {
 		go bot.LogEvent(ctx, cbq.From, CategoryCallback, ActionRefreshCallback, cbq.Message.Chat.Type)
 	} else {
 		go bot.LogEvent(ctx, cbq.From, CategoryCallback, ActionRefreshCallback, LabelInlineMessage)
 	}
 
-	answer := tgbotapi.NewCallback(cbq.ID, "Etas updated!")
-
-	var wg sync.WaitGroup
-	wg.Add(2)
-
-	errs := make(chan error, 2)
-	go func() {
-		for err := range errs {
-			log.Errorf(ctx, "%v", err)
-		}
-	}()
-
-	go func() {
-		defer wg.Done()
-
-		_, err = bot.Telegram.Send(reply)
-		if err != nil {
-			errs <- err
-		}
-	}()
-
-	go func() {
-		defer wg.Done()
-
-		_, err := bot.Telegram.AnswerCallbackQuery(answer)
-		if err != nil {
-			errs <- err
-		}
-	}()
-
-	wg.Wait()
-	return nil
+	err = answerCallbackQuery(bot, reply, answer)
+	return err
 }
 
 // EtaCallbackHandler handles callback queries from eta messages from old versions of the bot for
