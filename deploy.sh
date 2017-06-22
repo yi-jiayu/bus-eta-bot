dc65cc1dad556fdf03e2b7213f47d0913a4f74a1
Jiayu Yi <yi-jiayu@users.noreply.github.com>
2017-06-22 09:21:54 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 ENV="${1:-staging}"
 
 gcloud auth activate-service-account --key-file travis-ci-service-account.json
-gcloud --quiet app --project bus-eta-bot deploy ${ENV}.app.yaml --version $(sed "s/\./-/g" <<< $(git describe --tags))
+gcloud --quiet app --project bus-eta-bot deploy ${ENV}.app.yaml --version $(git describe --tags | sed "s/\./-/g")
